706005270bd2d4872d0cfc068a82ad0e35deb835
kunal1729 <102245342+kunal1729@users.noreply.github.com>
2024-07-15 19:01:04 +0530

    Update Mails.js

--- app/components/Mails.js
+++ app/components/Mails.js
@@ -143,17 +143,17 @@ const Mails = ({type}) => {
   return (
     <div className='w-full ml-2 mb-2 border-2 shadow-lg rounded-xl bg-white  mr-4'>
         <div className='flex p-4 justify-between items-center rounded-t-xl '>
-            <div className='font-bold space-x-4 pl-2 flex items-center'>
+            <div className='font-bold hidden space-x-4 pl-2 flex items-center'>
                 <Tooltip title = 'Select All'>
                     <input onChange={handleSelectAll} type='checkbox'/>
                 </Tooltip>
-                <button onClick={handleRefresh} className={`${refresh ? "p-2 bg-slate-300 rounded-full" : ""}`}>
+                <button onClick={handleRefresh} className={`"hidden" ${refresh ? "p-2 bg-slate-300 rounded-full" : ""}`}>
                     <Tooltip title = 'Refresh'>
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-rotate-cw"><path d="M21 12a9 9 0 1 1-9-9c2.52 0 4.93 1 6.74 2.74L21 8"/><path d="M21 3v5h-5"/></svg>
                     </Tooltip>
                 </button>
             </div>
-            {isSelected ? (
+            {selectedIds.length  ? (
             <div className='flex items-center space-x-3'>
                 <button onClick={moveToSpam}>
                     <Tooltip title = 'Report Spam'>
